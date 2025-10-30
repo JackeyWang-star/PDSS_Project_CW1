ce3717599cc1eefea3f904f261e24fa0645446d9
JackeyWang-star <1638534073@qq.com>
2025-10-30 20:23:59 +0000

    将转化方法封装到Converter类中

---
+++ src/main/scala/Converter.scala
@@ -0,0 +1,161 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import scala.io.Source
+
+class Converter {
+  private val conf: SparkConf = new SparkConf()
+    .setAppName("CW1") // Set your application's name
+    .setMaster("local[*]") // Use all cores of the local machine
+    .set("spark.ui.enabled", "false")
+  val sc: SparkContext = new SparkContext(conf)
+
+  def SMToCOO (address: String): (RDD[Int], RDD[Int], RDD[Double]) = {
+    /*
+    Row:       ,List(0, 0, 1, 3)
+    Col:       ,List(0, 2, 1, 3)
+    Value:     ,List(4.0, 9.0, 7.0, 5.0)
+     */
+    var rowIndices = List[Int]()
+    var colIndices = List[Int]()
+    var values = List[Double]()
+    val source = Source.fromFile(address)
+    try {
+      val lines = source.getLines().toList
+
+      for ((line, i) <- lines.zipWithIndex) {
+        val entries = line.replace("\uFEFF", "").split(",").map(_.trim.toDouble)
+        for ((v, j) <- entries.zipWithIndex) {
+          if (v != 0) {
+            rowIndices ::= i
+            colIndices ::= j
+            values ::= v
+          }
+        }
+      }
+    } finally {
+      source.close()
+    }
+    println("Row:       ", rowIndices.reverse)
+    println("Col:       ", colIndices.reverse)
+    println("Value:     ", values.reverse)
+    (sc.parallelize(rowIndices.reverse), sc.parallelize(colIndices.reverse), sc.parallelize(values.reverse))
+
+  }
+
+  def SMToCSC (address: String): (RDD[Int], RDD[Int], RDD[Double]) = {
+    /*
+    Row:       ,List(0, 1, 0, 3)
+    ColOffset: ,List(0, 1, 2, 3, 4)
+    Value:     ,List(4, 7, 9, 5)
+     */
+    var rowIndices = List[Int]()
+    var colOffset = List[Int]()
+    var values = List[Double]()
+    var indices: Int = 0
+    val source = Source.fromFile(address)
+    val matrix = source.getLines().map(_.replace("\uFEFF", "").split(",").map(_.trim.toDouble)).toArray
+
+    val numOFrows = matrix.length
+    val numOFcol = matrix(0).length
+    try {
+      for (i <- 0 until  numOFcol){
+        colOffset ::= indices
+        for (j <- 0 until numOFrows){
+          if (matrix(j)(i) != 0){
+            indices += 1
+            rowIndices ::= j
+            values ::= matrix(j)(i)
+          }
+        }
+      }
+      colOffset ::= indices
+    } finally {
+      source.close()
+    }
+    println("Row:       ", rowIndices.reverse)
+    println("Col:       ", colOffset.reverse)
+    println("Value:     ", values.reverse)
+    (sc.parallelize(rowIndices.reverse), sc.parallelize(colOffset.reverse), sc.parallelize(values.reverse))
+  }
+
+  def SMToCSR (address: String): (RDD[Int], RDD[Int], RDD[Double]) = {
+    /*
+    RowOffset: ,List(0, 2, 3, 3, 4)
+    Col:       ,List(0, 2, 1, 3)
+    Value:     ,List(4.0, 9.0, 7.0, 5.0)
+     */
+    var rowOffset = List[Int]()
+    var colIndices = List[Int]()
+    var values = List[Double]()
+    var indices: Int = 0
+    val source = Source.fromFile(address)
+
+    try {
+      val lines = source.getLines().toList
+
+      for ((line, i) <- lines.zipWithIndex) {
+        rowOffset ::= indices
+        val entries = line.replace("\uFEFF", "").split(",").map(_.trim.toDouble)
+        for ((v, j) <- entries.zipWithIndex) {
+          if (v != 0) {
+            colIndices ::= j
+            values ::= v
+            indices += 1
+          }
+        }
+      }
+      rowOffset ::= indices
+    } finally {
+      source.close()
+    }
+    println("Row:       ", rowOffset.reverse)
+    println("Col:       ", colIndices.reverse)
+    println("Value:     ", values.reverse)
+    (sc.parallelize(rowOffset.reverse), sc.parallelize(colIndices.reverse), sc.parallelize(values.reverse))
+  }
+
+  def SMToSELL (address: String, sliceHigh: Int): (RDD[Int], RDD[Int], RDD[Double]) = {
+    /*
+    sliceHigh = 2
+    sliceNum:  ,List(0, 4, 6)
+    Col:       ,List(0, 1, 2,-1,-1, 3)
+    Value:     ,List(4, 7, 9, *, *, 5)
+     */
+    val source = Source.fromFile(address)
+    val matrix = source.getLines().map(_.replace("\uFEFF", "").split(",").map(_.trim.toDouble)).toArray
+
+    val numOFrows = matrix.length
+    val numOFcol = matrix(0).length
+    val numOFslice = (numOFrows + sliceHigh - 1) / sliceHigh
+
+    var values = List[Double]()
+    var colIdices = List[Int]()
+    var sliceNum = List[Int]()
+    sliceNum ::= 0
+
+    for (i <- 0 until numOFslice){
+      val start = i * sliceHigh
+      val end = Math.min(start + sliceHigh, numOFrows)
+      val slice = matrix.slice(start, end)
+      val MaxWith = slice.map(_.count(_ != 0)).max
+
+      for (j <- slice){
+        val nz = j.zipWithIndex.filter(_._1 != 0)
+        val res = MaxWith - nz.length
+        values = values ++ (nz.map(_._1) ++ List.fill(res)(0.0))
+        colIdices = colIdices ++ (nz.map(_._2) ++ List.fill(res)(-1))
+      }
+      sliceNum ::= values.length
+    }
+
+    println("Slice:     ", sliceNum.reverse)
+    println("Col:       ", colIdices)
+    println("Value:     ", values)
+
+    (sc.parallelize(sliceNum.reverse), sc.parallelize(colIdices), sc.parallelize(values))
+  }
+
+  //  def ReadSV (address: String): (RDD[Int], RDD[Double]) = {
+  //
+  //  }
+}
--- src/main/scala/ReadFile.scala
+++ src/main/scala/ReadFile.scala
@@ -4,6 +4,7 @@ SMToCSC (address: String): (RDD[Int], RDD[Int], RDD[Double])
 SMToCSR (address: String): (RDD[Int], RDD[Int], RDD[Double])
 这三个方法包含了读取CSV文件并转换成COO，CSC，CSR这三种保存形式。结构会被保存在三个PDD中返回。
 运行PDD时会输出很多红色日志，目前我还没找到把它关闭的方法。
+运行前需要注意JAVA CDK的版本，最好使用JAVA8或者JAVA11
 */
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
@@ -11,127 +12,18 @@ import scala.io.Source
 import org.apache.log4j.{Level, Logger}
 
 object ReadCSV {
-  //Global variables
-  private val conf: SparkConf = new SparkConf()
-    .setAppName("CW1") // Set your application's name
-    .setMaster("local[*]") // Use all cores of the local machine
-    .set("spark.ui.enabled", "false")
-  val sc: SparkContext = new SparkContext(conf)
-
-
-  def SMToCOO (address: String): (RDD[Int], RDD[Int], RDD[Double]) = {
-    /*
-    Row:       ,List(0, 0, 1, 3)
-    Col:       ,List(0, 2, 1, 3)
-    Value:     ,List(4.0, 9.0, 7.0, 5.0)
-     */
-    var rowIndices = List[Int]()
-    var colIndices = List[Int]()
-    var values = List[Double]()
-    val source = Source.fromFile(address)
-    try {
-      val lines = source.getLines().toList
-
-      for ((line, i) <- lines.zipWithIndex) {
-        val entries = line.replace("\uFEFF", "").split(",").map(_.trim.toDouble)
-        for ((v, j) <- entries.zipWithIndex) {
-          if (v != 0) {
-            rowIndices ::= i
-            colIndices ::= j
-            values ::= v
-          }
-        }
-      }
-    } finally {
-      source.close()
-    }
-    println("Row:       ", rowIndices.reverse)
-    println("Col:       ", colIndices.reverse)
-    println("Value:     ", values.reverse)
-    (sc.parallelize(rowIndices.reverse), sc.parallelize(colIndices.reverse), sc.parallelize(values.reverse))
-
-  }
-
-  def SMToCSC (address: String): (RDD[Int], RDD[Int], RDD[Double]) = {
-    /*
-    Row:       ,List(0, 1, 0, 3)
-    ColOffset: ,List(0, 1, 2, 3, 4)
-    Value:     ,List(4, 7, 9, 5)
-     */
-    var rowIndices = List[Int]()
-    var colOffset = List[Int]()
-    var values = List[Double]()
-    var indices: Int = 0
-    val source = Source.fromFile(address)
-    val matrix = source.getLines().map(_.replace("\uFEFF", "").split(",").map(_.trim.toDouble)).toArray
-
-    val numOFrows = matrix.length
-    val numOFcol = matrix(0).length
-    try {
-      for (i <- 0 until  numOFcol){
-        colOffset ::= indices
-        for (j <- 0 until numOFrows){
-          if (matrix(j)(i) != 0){
-            indices += 1
-            rowIndices ::= j
-            values ::= matrix(j)(i)
-          }
-        }
-      }
-      colOffset ::= indices
-    } finally {
-      source.close()
-    }
-    println("Row:       ", rowIndices.reverse)
-    println("Col:       ", colOffset.reverse)
-    println("Value:     ", values.reverse)
-    (sc.parallelize(rowIndices.reverse), sc.parallelize(colOffset.reverse), sc.parallelize(values.reverse))
-  }
-
-  def SMToCSR (address: String): (RDD[Int], RDD[Int], RDD[Double]) = {
-    /*
-    RowOffset: ,List(0, 2, 3, 3, 4)
-    Col:       ,List(0, 2, 1, 3)
-    Value:     ,List(4.0, 9.0, 7.0, 5.0)
-     */
-    var rowOffset = List[Int]()
-    var colIndices = List[Int]()
-    var values = List[Double]()
-    var indices: Int = 0
-    val source = Source.fromFile(address)
-
-    try {
-      val lines = source.getLines().toList
-
-      for ((line, i) <- lines.zipWithIndex) {
-        rowOffset ::= indices
-        val entries = line.replace("\uFEFF", "").split(",").map(_.trim.toDouble)
-        for ((v, j) <- entries.zipWithIndex) {
-          if (v != 0) {
-            colIndices ::= j
-            values ::= v
-            indices += 1
-          }
-        }
-      }
-      rowOffset ::= indices
-    } finally {
-      source.close()
-    }
-    println("Row:       ", rowOffset.reverse)
-    println("Col:       ", colIndices.reverse)
-    println("Value:     ", values.reverse)
-    (sc.parallelize(rowOffset.reverse), sc.parallelize(colIndices.reverse), sc.parallelize(values.reverse))
-  }
-
   def main(args: Array[String]): Unit = {
     // Set log level
     org.apache.log4j.Logger.getLogger("org").setLevel(org.apache.log4j.Level.ERROR)
     org.apache.log4j.Logger.getLogger("akka").setLevel(org.apache.log4j.Level.ERROR)
 
+    val Con = new Converter
+
     val filePath = "D:\\EdinburghUniversity\\Git_Project\\PDSS_CW1\\Data\\SMConverter_test.csv"
     println("The file path is: " + filePath)
-    val (row1, col1, value1) = SMToCOO(filePath)
+
+    //测试COO转换
+    val (row1, col1, value1) = Con.SMToCOO(filePath)
     println("Read the Sparse matrix in file and saved in COO form:")
     println("row:   ")
     row1.toLocalIterator.foreach(println)
@@ -140,7 +32,8 @@ object ReadCSV {
     println("value:  ")
     value1.toLocalIterator.foreach(println)
 
-    val (row2, col2, value2) = SMToCSR(filePath)
+    //测试CSR转换
+    val (row2, col2, value2) = Con.SMToCSR(filePath)
     println("Read the Sparse matrix in file and saved in CSR form:")
     println("rowOffset:   ")
     row2.toLocalIterator.foreach(println)
@@ -148,9 +41,9 @@ object ReadCSV {
     col2.toLocalIterator.foreach(println)
     println("value:  ")
     value2.toLocalIterator.foreach(println)
-    sc.stop()
 
-    val (row3, col3, value3) = SMToCSC(filePath)
+    //测试CSC转换
+    val (row3, col3, value3) = Con.SMToCSC(filePath)
     println("Read the Sparse matrix in file and saved in CSR form:")
     println("row:   ")
     row3.toLocalIterator.foreach(println)
@@ -158,6 +51,16 @@ object ReadCSV {
     col3.toLocalIterator.foreach(println)
     println("value:  ")
     value3.toLocalIterator.foreach(println)
-    sc.stop()
+
+    //测试SELL转换
+    val (row4, col4, value4) = Con.SMToSELL(filePath, 2)
+    println("Read the Sparse matrix in file and saved in SELL form:")
+    println("Slice:   ")
+    row4.toLocalIterator.foreach(println)
+    println("colOffset:   ")
+    col4.toLocalIterator.foreach(println)
+    println("value:  ")
+    value4.toLocalIterator.foreach(println)
+
   }
 }
